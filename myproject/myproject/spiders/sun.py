9fcef2706110930308dad1f5f019e423bd2f24f2
sayid0927 <13113639501@163.com>
2017-10-04 15:53:29 +0000

    add new files

--- myproject/myproject/spiders/sun.py
+++ myproject/myproject/spiders/sun.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -11,20 +11,27 @@ class SunSpider(CrawlSpider):
     start_urls = ['https://t66y.com/thread0806.php?fid=20&page=1']
 
     rules = (
-        Rule(LinkExtractor(allow=r'page=\d+'),process_links='deal_links'),
+        Rule(LinkExtractor(allow=r'page=\d+')),
         Rule(LinkExtractor(allow=r'htm_data/\d+/\d+/\d+.html'),callback ='parse_item'),
     )
     
-    def deal_links(self,links):
-        for link in links:
-            print(link.url) 
+#    def deal_links(self,links):
+#        if len(links)>0:
+#           for link in links: 
+#               print(link.url)
+#        else:
+#           return None
+          
 
 
     def parse_item(self, response):
-        print (response.url)
+        print ('respone.utl===>>>'+response.url)
         item = MyprojectItem()
-        item['title'] = response.xpath('//tr[@class="tr1 do_not_catch"]//h4/text()').extract()
-        item['content'] = response.xpath('//div[@class="tpc_content do_not_catch"]/text()').extract()
+        item['title'] = response.xpath('//tr[@class="tr1 do_not_catch"]//h4/text()').extract()[0]
+        print('item>> title===>>>'+item['title'])
+        content_list = response.xpath('//div[@class="tpc_content do_not_catch"]/text()').extract()
+        item['content']= ''.join(content_list)
+        print('item>>content===>>>'+item['content'])
         item['url'] = response.url
         yield item
 
